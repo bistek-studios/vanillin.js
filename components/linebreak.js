82ac81151efcce73d95bf38872df06d7f8f9c2a5
bismuth <70106649+bismuthnickel@users.noreply.github.com>
2024-10-24 00:46:01 -0400

    testing out more components

---
+++ components/linebreak.js
@@ -0,0 +1,7 @@
+export let Br = (x) => {
+    let maxxing = "";
+    for (let i = 0; i < x; i++) {
+        maxxing = maxxing + "<br>"
+    };
+    return maxxing;
+};
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { App } from "./vanillin.js";
 import { Counter } from "./components/counter.js";
 import { InputThingy } from "./components/input.js";
+import { Br } from "./components/linebreak.js";
 
 App(() => {
-    return `<!DOCTYPE html><html><head><title>fard</title></head><body>${Counter()}<br><br>${InputThingy()}</body></html>`;
+    return `<!DOCTYPE html><html><head><title>fard</title></head><body>${Counter()}${Br(2)}${InputThingy()}</body></html>`;
 });
